07de7afee4e9a2fc16720382b42dd0752c3f04d9
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2024-11-04 23:00:30 +0100

    Also handle managed dependencies

--- src/test/java/org/operaton/rewrite/MigrateDependenciesTest.java
+++ src/test/java/org/operaton/rewrite/MigrateDependenciesTest.java
@@ -1,5 +1,6 @@
 package org.operaton.rewrite;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -88,5 +89,60 @@ public class MigrateDependenciesTest implements RewriteTest {
                 </dependencies>
             </project>
             """)));
+    }
+
+    @Test
+    void migrateCamundaEngineWithManagedVersion() {
+        rewriteRun(
+          mavenProject("project", pomXml(
+            """
+            <project>
+               <groupId>org.operaton.test</groupId>
+               <artifactId>test-app</artifactId>
+               <version>1</version>
+               <dependencyManagement>
+                 <dependencies>
+                   <dependency>
+                     <groupId>org.camunda.bpm</groupId>
+                     <artifactId>camunda-bom</artifactId>
+                     <version>7.22.0</version>
+                     <scope>import</scope>
+                     <type>pom</type>
+                   </dependency>
+                 </dependencies>
+               </dependencyManagement>
+               <dependencies>
+                 <dependency>
+                   <groupId>org.camunda.bpm</groupId>
+                   <artifactId>camunda-engine</artifactId>
+                 </dependency>
+               </dependencies>
+           </project>
+           """,
+
+            """
+            <project>
+               <groupId>org.operaton.test</groupId>
+               <artifactId>test-app</artifactId>
+               <version>1</version>
+               <dependencyManagement>
+                 <dependencies>
+                   <dependency>
+                     <groupId>org.operaton.bpm</groupId>
+                     <artifactId>operaton-bom</artifactId>
+                     <version>1.0.0-beta-1</version>
+                     <scope>import</scope>
+                     <type>pom</type>
+                   </dependency>
+                 </dependencies>
+               </dependencyManagement>
+               <dependencies>
+                 <dependency>
+                   <groupId>org.operaton.bpm</groupId>
+                   <artifactId>operaton-engine</artifactId>
+                 </dependency>
+               </dependencies>
+           </project>
+           """)));
     }
 }
