895c502575f7c289222dc7060e6bed7836f42fbb
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2024-11-06 22:15:38 +0100

    Migrate properties in Maven files (#2)
    * Add (failing) test for migrating properties
    
    * Also handle managed dependencies
    
    * Cleaning up, removing depdencencies from the template
    
    * feat: Rewrite simple Maven dependencies and managed dependencies

--- src/test/java/org/operaton/rewrite/MigrateDependenciesTest.java
+++ src/test/java/org/operaton/rewrite/MigrateDependenciesTest.java
@@ -1,5 +1,6 @@
 package org.operaton.rewrite;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -47,5 +48,102 @@ public class MigrateDependenciesTest implements RewriteTest {
                 </dependencies>
             </project>
             """)));
+    }
+
+    @Test
+    @Disabled("Solve in a separate issue #13")
+    void migrateCamundaEngineWithVersionInProperty() {
+        rewriteRun(
+          mavenProject("project", pomXml(
+            """
+            <project>
+               <groupId>org.operaton.test</groupId>
+               <artifactId>test-app</artifactId>
+               <version>1</version>
+               <properties>
+                 <camunda.version>7.22.0</camunda.version>
+               </properties>
+               <dependencies>
+                 <dependency>
+                   <groupId>org.camunda.bpm</groupId>
+                   <artifactId>camunda-engine</artifactId>
+                   <version>${camunda.version}</version>
+                 </dependency>
+               </dependencies>
+           </project>
+           """,
+
+            """
+             <project>
+                <groupId>org.operaton.test</groupId>
+                <artifactId>test-app</artifactId>
+                <version>1</version>
+               <properties>
+                 <camunda.version>1.0.0-beta-1</camunda.version>
+               </properties>
+                <dependencies>
+                  <dependency>
+                    <groupId>org.operaton.bpm</groupId>
+                    <artifactId>operaton-engine</artifactId>
+                    <version>${camunda.version}</version>
+                  </dependency>
+                </dependencies>
+            </project>
+            """)));
+    }
+
+    @Test
+    void migrateCamundaEngineWithManagedVersion() {
+        rewriteRun(
+          mavenProject("project", pomXml(
+            """
+            <project>
+               <groupId>org.operaton.test</groupId>
+               <artifactId>test-app</artifactId>
+               <version>1</version>
+               <dependencyManagement>
+                 <dependencies>
+                   <dependency>
+                     <groupId>org.camunda.bpm</groupId>
+                     <artifactId>camunda-bom</artifactId>
+                     <version>7.22.0</version>
+                     <scope>import</scope>
+                     <type>pom</type>
+                   </dependency>
+                 </dependencies>
+               </dependencyManagement>
+               <dependencies>
+                 <dependency>
+                   <groupId>org.camunda.bpm</groupId>
+                   <artifactId>camunda-engine</artifactId>
+                 </dependency>
+               </dependencies>
+           </project>
+           """,
+
+            """
+            <project>
+               <groupId>org.operaton.test</groupId>
+               <artifactId>test-app</artifactId>
+               <version>1</version>
+               <dependencyManagement>
+                 <dependencies>
+                   <dependency>
+                     <groupId>org.operaton.bpm</groupId>
+                     <artifactId>operaton-bom</artifactId>
+                     <version>1.0.0-beta-1</version>
+                     <scope>import</scope>
+                     <type>pom</type>
+                   </dependency>
+                 </dependencies>
+               </dependencyManagement>
+               <dependencies>
+                 <dependency>
+                   <groupId>org.operaton.bpm</groupId>
+                   <artifactId>operaton-engine</artifactId>
+                 </dependency>
+               </dependencies>
+           </project>
+           """)));
     }
 }
