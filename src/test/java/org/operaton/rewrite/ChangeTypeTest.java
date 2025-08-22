46892a151ef55d93efab323f7a651df1c5f9c530
alexh141048 <145465437+alexh141048@users.noreply.github.com>
2025-08-22 15:11:34 +0200

    feat(recipe):  added changetype recipe to migrate the usage of camunda types (#132)
    * added recipeList for change usage of camunda types
    * added test: migrate a implementation of a camunda interface
    * Make `ChangeTypeTest` package-private
    * Document `ChangeType` recipe in README.md
    
    ---------
    
    Co-authored-by: Alexander Horbach <alexander.horbach@vitroconnect.com>
    Co-authored-by: Lars Uffmann <lars.uffmann@gmail.com>

---
+++ src/test/java/org/operaton/rewrite/ChangeTypeTest.java
@@ -0,0 +1,48 @@
+package org.operaton.rewrite;
+
+import static org.openrewrite.java.Assertions.java;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+class ChangeTypeTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("org.operaton.rewrite.ChangeType", "org.operaton.rewrite.ChangePackage");
+    }
+
+    @Test
+    void testChangeInterface() {
+        rewriteRun(
+          java("""
+            package org.foo.example;
+            
+            import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+            import org.camunda.bpm.spring.boot.starter.configuration.CamundaProcessEngineConfiguration;
+            
+            public class Test implements CamundaProcessEngineConfiguration {
+            
+                @Override
+                public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+                    System.out.println("test pre-init");
+                }
+            }""", """
+            package org.foo.example;
+            
+            import org.operaton.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+            import org.operaton.bpm.spring.boot.starter.configuration.OperatonProcessEngineConfiguration;
+            
+            public class Test implements OperatonProcessEngineConfiguration {
+            
+                @Override
+                public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+                    System.out.println("test pre-init");
+                }
+            }"""
+          )
+        );
+    }
+
+}
